9e8e9c1601c24d3662b848743d644929203182df
Roman Gailler <r.gailler@simplytest.de>
2025-08-01 14:53:13 +0200

    send json data to db

--- src/test/java/steps/PauseSteps.java
+++ src/test/java/steps/PauseSteps.java
@@ -11,14 +11,26 @@ import entity.TestContext;
 public class PauseSteps {
     public static String scenarioName = "";
 
+    // Beispiel-JSONs
+    private static final String JSON_EMPTY = "{}";
+    private static final String JSON_ONE_PARAM = "{\"foo\":\"bar\"}";
+    private static final String JSON_TWO_PARAMS = "{\"foo\":\"bar\",\"x\":123}";
+    private static final String JSON_THREE_PARAMS = "{\"foo\":\"bar\",\"x\":123,\"flag\":true}";
+
     @When("ich den Test pausieren möchte bis {string}")
     public void ich_den_test_pausieren_moechte_bis(String resumeTime) {
         System.out.println("Step: ich den Test pausieren möchte bis " + resumeTime);
         System.out.println("Szenario: " + scenarioName);
+
+        // Beispiel: JSON mit 3 Parametern anhängen
+        String variablesJson = JSON_THREE_PARAMS;
+        System.out.println("Beispiel-JSON: " + variablesJson);
+
         TestContext context = TestContext.builder()
             .testName(scenarioName)
             .trigger("resume")
             .resumeTimestamp(LocalDateTime.parse(resumeTime))
+            .variablesJson(variablesJson)
             .build();
 
         WebClient.create("http://localhost:8080")
